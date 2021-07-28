253632d554851c381f402c392a95cee5c1f62293
Pavan Mundada <pavanpmundada5793@gmail.com>
2021-07-28 09:39:53 +0530

    added static image in the background

--- lottie_animation/App.js
+++ lottie_animation/App.js
@@ -9,7 +9,7 @@ import {
   TouchableOpacity,
 } from 'react-native';
 import LottieView from 'lottie-react-native';
-import pharma from './assets/pharma.json';
+import helicopter from './assets/helicopter.json';
 
 const image = {
   uri: 'https://img.buzzfeed.com/buzzfeed-static/static/2020-02/14/21/enhanced/553ef1e26c9a/enhanced-372-1581714965-4.png',
@@ -49,61 +49,96 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <LottieView style={styles.animationContainer}>
-        {/* <TouchableOpacity onPress={this.alertBox} activeOpacity={0.9}> */}
-        {/* <ImageBackground
-            source={image}
-            resizeMode="center"
-            style={styles.image}> */}
-        <LottieView
-          ref={animation => {
-            this.animation = animation;
-          }}
-          resizeMode="contain"
-          speed={1} //we can control the speed of an animation.Sending a negative value will reverse the animation
-          progress={0} //A number between 0 and 1, or an Animated number between 0 and 1. This number represents the normalized progress of the animation. If you update this prop, the animation will correspondingly update to the frame at that progress value. This prop is not required if you are using the imperative API.
-          style={{
-            width: 400,
-            height: 400,
-            backgroundColor: '#eee',
-          }}
-          // source={require('./assets/cycle.json')}
-          source={pharma}
-          loop={true}
+      <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+        <LottieView style={styles.animationContainer}>
+          {/* <TouchableOpacity onPress={this.alertBox} activeOpacity={0.9}> */}
 
-          // OR find more Lottie files @ https://lottiefiles.com/featured
-          // Just click the one you like, place that file in the 'assets' folder to the left, and replace the above 'require' statement
-        />
-        {/* </ImageBackground> */}
-        <View style={styles.buttonContainer}>
-          <Button style={styles.playStyle} title="" onPress={this.alertBox} />
-          {/* <Button title="Reset" onPress={this.resetAnimation} /> */}
-        </View>
+          <LottieView
+            ref={animation => {
+              this.animation = animation;
+            }}
+            resizeMode="contain"
+            speed={1} //we can control the speed of an animation.Sending a negative value will reverse the animation
+            progress={0} //A number between 0 and 1, or an Animated number between 0 and 1. This number represents the normalized progress of the animation. If you update this prop, the animation will correspondingly update to the frame at that progress value. This prop is not required if you are using the imperative API.
+            style={{
+              width: 400,
+              height: 215,
+              backgroundColor: '#eee',
+            }}
+            // source={require('./assets/cycle.json')}
+            source={helicopter}
+            loop={true}
 
-        {/* </TouchableOpacity> */}
-      </LottieView>
+            // OR find more Lottie files @ https://lottiefiles.com/featured
+            // Just click the one you like, place that file in the 'assets' folder to the left, and replace the above 'require' statement
+          />
+          {/* <LottieView
+            ref={animation => {
+              this.animation = animation;
+            }}
+            // resizeMode=""
+            speed={1} //we can control the speed of an animation.Sending a negative value will reverse the animation
+            progress={0} //A number between 0 and 1, or an Animated number between 0 and 1. This number represents the normalized progress of the animation. If you update this prop, the animation will correspondingly update to the frame at that progress value. This prop is not required if you are using the imperative API.
+            style={{
+              width: 400,
+              height: 215,
+              backgroundColor: '#eee',
+            }}
+            // source={require('./assets/cycle.json')}
+            source={pharma}
+            loop={true}
+
+            // OR find more Lottie files @ https://lottiefiles.com/featured
+            // Just click the one you like, place that file in the 'assets' folder to the left, and replace the above 'require' statement
+          /> */}
+          <View style={styles.buttonContainer}>
+            <Button
+              style={styles.pharmaStyle}
+              title=""
+              onPress={this.alertBox}
+            />
+          </View>
+
+          <View style={styles.jetsContainer}>
+            <Button style={styles.playStyle} title="" onPress={this.alertBox} />
+          </View>
+
+          <View style={styles.sunContainer}>
+            <Button style={styles.playStyle} title="" onPress={this.alertBox} />
+          </View>
+
+          <View style={styles.benchmarkContainer}>
+            <Button style={styles.playStyle} title="" onPress={this.alertBox} />
+          </View>
+
+          <View style={styles.clinicContainer}>
+            <Button style={styles.playStyle} title="" onPress={this.alertBox} />
+          </View>
+
+          {/* </TouchableOpacity> */}
+        </LottieView>
+      </ImageBackground>
     );
   }
 }
 
 const styles = StyleSheet.create({
   animationContainer: {
-    backgroundColor: '#fff',
+    // backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
     flex: 1,
   },
-  // image: {
-  //   flex: 1,
-  //   justifyContent: 'center',
-  // },
+  image: {
+    flex: 1,
+    justifyContent: 'center',
+  },
   buttonContainer: {
     // flexDirection: 'row',
     // justifyContent: 'center',
     // position: 'absolute',
     // paddingTop: 30,
     // marginRight: 40,
-    margin: 50,
     position: 'absolute',
 
     top: 400,
@@ -116,8 +151,72 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     padding: 10,
     borderRadius: 100,
+    backgroundColor: 'blue',
+    // color: 'tomato',
+  },
+
+  jetsContainer: {
+    position: 'absolute',
+
+    top: 200,
+    left: 100,
+    // width: 100,
+    // height: 50,
+    width: 25,
+    height: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+    borderRadius: 100,
     backgroundColor: 'red',
     // color: 'tomato',
+  },
+
+  benchmarkContainer: {
+    position: 'absolute',
+
+    top: 300,
+    left: 50,
+    // width: 100,
+    // height: 50,
+    width: 25,
+    height: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 100,
+    backgroundColor: 'orange',
+    // color: 'tomato',
+  },
+
+  sunContainer: {
+    position: 'absolute',
+
+    top: 150,
+    left: 250,
+    // width: 100,
+    // height: 50,
+    width: 25,
+    height: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 100,
+    backgroundColor: 'black',
+    // color: 'tomato',
+  },
+
+  clinicContainer: {
+    position: 'absolute',
+
+    top: 375,
+    left: 200,
+    // width: 100,
+    // height: 50,
+    width: 25,
+    height: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 100,
+    backgroundColor: 'brown',
   },
   playStyle: {
     // position: 'absolute',
